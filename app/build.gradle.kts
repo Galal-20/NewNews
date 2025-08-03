a91537e7f7603311842c35bb71b11c59c9e0752b
Galal-20 <ga71387@gmail.com>
2025-08-03 11:15:23 +0300

    Project Structure. Splash Screen. Navigation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android") version "2.52"
+
 }
 
 android {
@@ -33,6 +36,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+    hilt {
+        enableAggregatingTask = false
+    }
 }
 
 dependencies {
@@ -42,7 +52,45 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Hilt dependencies
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-android-compiler:2.52")
+    //implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // Explicitly add javapoet to ensure compatibility
+    kapt("com.squareup:javapoet:1.13.0") // Use 1.13.0, as it's stable with Hilt 2.52
+
+
+
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.35.0-alpha")
+
+    implementation ("androidx.security:security-crypto:1.1.0-alpha03")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation ("com.google.android.material:material:1.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+    implementation("com.airbnb.android:lottie:6.6.7")
+
+    implementation("androidx.navigation:navigation-compose:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("com.google.accompanist:accompanist-pager:0.36.0")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.32.0") // For indicators
+
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+    // placeholder for loading.
+    implementation("com.valentinilk.shimmer:compose-shimmer:1.3.3")
+
+}
+
+kapt{
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/galal/newnews/utils/networkListener.kt
@@ -0,0 +1,4 @@
+package com.galal.newnews.utils
+
+class networkListener {
+}
